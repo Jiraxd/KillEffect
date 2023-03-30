8f91630addcd7e4eb7929342125b77c0b2e1d137
Jiraxd <123981279+Jiraxd@users.noreply.github.com>
2023-03-30 16:54:43 +0200

    First version

---
+++ KillEffect/KillEffect/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using Rocket.API;
+
+namespace KillEffect
+{
+	
+	public class Config : IRocketPluginConfiguration, IDefaultable
+	{
+		
+		public void LoadDefaults()
+		{
+			this.KillerEffectId = 123;
+		}
+		public ushort KillerEffectId;
+
+	}
+}
---
+++ KillEffect/KillEffect/Main.cs
@@ -0,0 +1,41 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.NetTransport;
+using SDG.Unturned;
+using Steamworks;
+
+namespace KillEffect
+{
+	
+	public class Main : RocketPlugin<Config>
+	{
+	
+	
+		internal static Config Config
+		{
+			get
+			{
+				return Main.Instance.Configuration.Instance;
+			}
+		}
+
+	
+		protected override void Load()
+		{
+			Main.Instance = this;
+			PlayerLife.onPlayerDied += this.OnPlayerDied;
+		}
+
+
+		private void OnPlayerDied(PlayerLife sender, EDeathCause cause, ELimb limb, CSteamID murderer)
+		{
+			UnturnedPlayer player = UnturnedPlayer.FromPlayer(sender.player);
+			if (murderer != CSteamID.Nil && murderer != Provider.server && murderer != player.CSteamID)
+			{
+                ITransportConnection transportConnection = Provider.findTransportConnection(murderer);
+                EffectManager.sendUIEffect(Main.Config.KillerEffectId, 8548, transportConnection, true);
+			}
+		}
+		public static Main Instance;
+	}
+}
